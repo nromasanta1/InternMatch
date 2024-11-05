6c7c2d1cfa60689cfbc7c67ba8d0d0147707fda1
RUI KAN <ruikan0809@gmail.com>
2024-11-05 22:37:01 +1300

    redo seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,7 @@ Internship.destroy_all
 puts "Emptying USERS database..."
 User.destroy_all
 puts "Creating EMPLOYER 1 - Abby Chamber..."
-User.create(
-  id: 1,
+abby = User.create(
   email: "abbychamber@gmail.com",
   password: "123456",
   user_type: 0,
@@ -18,8 +17,7 @@ User.create(
   location: "Melbourne",
 )
 puts "Creating EMPLOYER 2 - Harry Potter..."
-User.create(
-  id: 2,
+harry = User.create(
   email: "harrypotter@gmail.com",
   password: "123456",
   user_type: 0,
@@ -31,8 +29,7 @@ User.create(
 )
 
 puts "Creating APPLICANT 1 - Homer Simpson..."
-User.create(
-  id: 3,
+homer = User.create(
   email: "homersimpson@gmail.com",
   password: "123456",
   user_type: 1,
@@ -44,8 +41,7 @@ User.create(
 )
 
 puts "Creating APPLICANT 2 - Bella Swan..."
-User.create(
-  id: 4,
+bella = User.create(
   email: "bellaswan@gmail.com",
   password: "123456",
   user_type: 1,
@@ -55,10 +51,12 @@ User.create(
   contact_number: "0400000000",
   location: "Melbourne",
 )
+employers = [abby, harry]
+applicants = [homer, bella]
 
 puts "Seeding internships..."
 10.times do
-  Internship.create(
+  internship = Internship.new(
     title: Faker::Job.title,
     role: Faker::Job.position,
     description: Faker::Company.catch_phrase,
@@ -66,8 +64,13 @@ puts "Seeding internships..."
     start_date: Faker::Date.between(from: 2.days.ago, to: Date.today),
     end_date: Faker::Date.between(from: '2025-01-30', to: '2025-07-31'),
     compensation: rand(0..10) * 1000,
-    user_id: rand(2)
+    user: employers.sample
   )
+  if internship.save
+    puts internship.title
+  else
+    puts internship.errors.full_messages
+  end
 end
 
 puts "Seeding internship applications..."
@@ -75,13 +78,13 @@ InternshipApplication.create(
   application_date: Faker::Date.between(from: 2.days.ago, to: Date.today),
   status: 0,
   internship_id: rand(Internship.first.id..Internship.last.id),
-  user_id: 3
+  user: applicants.sample
 )
 InternshipApplication.create(
   application_date: Faker::Date.between(from: 2.days.ago, to: Date.today),
   status: 0,
   internship_id: rand(Internship.first.id..Internship.last.id),
-  user_id: 4
+  user: applicants.sample
 )
 
 puts "Complete!"
