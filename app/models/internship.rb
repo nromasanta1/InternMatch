60218b111efbb2db942a2b87912f65e7ea9ca733
Ninna Romasanta <n.romasanta@outlook.com>
2024-11-07 19:51:57 +1100

    removed enums

--- app/models/internship.rb
+++ app/models/internship.rb
@@ -3,5 +3,4 @@ class Internship < ApplicationRecord
   has_many :internship_applications, dependent: :destroy
   validates :role, :description, :start_date, :end_date, :location, presence: true
   validates :title, presence: true, uniqueness: true
-  enum role: { employee: 0, employer: 1 }
 end
