0edd4764f85cb0f30880fd05043ab27370575c0f
Moyu Watanabe <koteko9222w@gmail.com>
2024-11-07 21:23:29 +1100

    Delete controller done

--- app/models/internship.rb
+++ app/models/internship.rb
@@ -1,6 +1,6 @@
 class Internship < ApplicationRecord
   belongs_to :user
   has_many :internship_applications, dependent: :destroy
-  validates :role, :description, :start_date, :end_date, :location, presence: true
+  validates :role, :description, :start_date, :end_date, :location, :compensation, presence: true
   validates :title, presence: true, uniqueness: true
 end
