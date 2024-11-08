b234127db109ceeaf4f86f5282d61daac9643315
Ninna Romasanta <n.romasanta@outlook.com>
2024-11-09 10:52:56 +1100

    edit

--- app/models/internship_application.rb
+++ app/models/internship_application.rb
@@ -3,7 +3,13 @@ class InternshipApplication < ApplicationRecord
   belongs_to :user
 
 <<<<<<< HEAD
+<<<<<<< HEAD
   enum :status, { pending: 0, accepted: 1, declined: 2 }
+=======
+  validates :user, uniqueness: { scope: :internship }
+  enum :status, { Pending: 0, Accepted: 1, Declined: 2 }
+
+>>>>>>> 36b4cd48aefba72801e4061bf0230a4ac83aa46f
 =======
   validates :user, uniqueness: { scope: :internship }
   enum :status, { Pending: 0, Accepted: 1, Declined: 2 }
