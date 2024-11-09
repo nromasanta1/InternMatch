63a52f31dbe93fb6b7232efd1b40866a40e4783b
Ivy Ta <131229797+ivy-ta@users.noreply.github.com>
2024-11-09 15:35:29 +1100

    seeds, color, buttons

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,6 +1,8 @@
 class RegistrationsController < Devise::RegistrationsController
   def create
     super
+    profile_picture = Cloudinary::Uploader.upload(params[:profile_picture],
+                                                  transformation: [{ width: 500, height: 500, crop: :fill }])
   end
 
   def sign_up_params
